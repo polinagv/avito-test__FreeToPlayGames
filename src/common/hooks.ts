0985a6b3446898ed1b3f2d2cc01a16221fa66b8e
polinagv <pol.grachewa@yandex.ru>
2023-08-29 18:00:11 +0300

    add custom hook for watching request loading status

--- src/common/hooks.ts
+++ src/common/hooks.ts
@@ -2,15 +2,24 @@ import { useEffect, useState } from 'react'
 import { Game } from './types.ts'
 import { getGamesList } from '../api/games.ts'
 
+type LoadingState = 'pending' | 'resolved' | 'rejected'
+export const useLoadingState = () => useState<LoadingState>('pending')
+
 export const useFetchGamesList = () => {
     const [games, setGames] = useState<Game[]>([])
+    const [gamesLoadingState, setGamesLoadingState] = useLoadingState()
 
     useEffect(() => {
-        getGamesList().then(({ data }) => {
-            setGames(data)
-            console.log(data)
-        })
-    }, [])
+        setGamesLoadingState('pending')
+        getGamesList()
+            .then(({ data }) => {
+                setGames(data)
+                setGamesLoadingState('resolved')
+            })
+            .catch(() => {
+                setGamesLoadingState('rejected')
+            })
+    }, [setGamesLoadingState])
 
-    return [games]
+    return { games, gamesLoadingState }
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,7 @@ import GameList from '../components/GameList.tsx'
 import { useFetchGamesList } from '../common/hooks.ts'
 
 const HomePage = () => {
-    const [games] = useFetchGamesList()
+    const { games } = useFetchGamesList()
 
     return <GameList games={games} />
 }
