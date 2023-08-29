af1c66387e43b1537f360a7ec18c08268cca1a25
polinagv <pol.grachewa@yandex.ru>
2023-08-29 22:49:56 +0300

    game not found component

---
+++ src/components/GameNotFound.tsx
@@ -0,0 +1,7 @@
+import { Empty } from 'antd'
+
+const GameNotFound = () => (
+    <Empty description={<span>Не удалось загрузить информацию об игре</span>} />
+)
+
+export default GameNotFound
--- src/pages/GameDetailsPage.tsx
+++ src/pages/GameDetailsPage.tsx
@@ -1,12 +1,18 @@
 import GameItem from '../components/GameItem.tsx'
 import { useFetchGameById } from '../common/hooks.ts'
 import { useParams } from 'react-router-dom'
+import { Spin } from 'antd'
+import GameNotFound from '../components/GameNotFound.tsx'
 
 const GameDetailsPage = () => {
     const { id } = useParams<{ id: string }>()
-    const { game } = useFetchGameById(id)
+    const { game, gameLoadingState } = useFetchGameById(id)
 
-    return game ? <GameItem game={game} /> : null
+    if (gameLoadingState === 'pending') {
+        return <Spin />
+    }
+
+    return game ? <GameItem game={game} /> : <GameNotFound />
 }
 
 export default GameDetailsPage
