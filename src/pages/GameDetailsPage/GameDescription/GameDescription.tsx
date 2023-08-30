5cf64bd2a59290c5e5796c33f98b0fe8e271e2b3
polinagv <pol.grachewa@yandex.ru>
2023-08-30 15:40:19 +0300

    move minimum_system_requirements into component

--- src/pages/GameDetailsPage/GameDescription/GameDescription.tsx
+++ src/pages/GameDetailsPage/GameDescription/GameDescription.tsx
@@ -2,36 +2,29 @@ import { Game } from 'common/types.ts'
 import { FC } from 'react'
 import DescriptionField from 'components/DescriptionField/DescriptionField.tsx'
 import { normalizeDate } from 'common/utils.ts'
-import styled from 'styled-components'
-
-const DesriptionWrap = styled.ul`
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-    max-width: 600px;
-`
+import { DescriptionWrap } from 'pages/GameDetailsPage/styles.ts'
+import SystemReqDescription from 'pages/GameDetailsPage/SystemReqDescription'
 
 type Props = {
     game: Game
 }
 const GameDescription: FC<Props> = ({ game }) => {
     return (
-        <DesriptionWrap>
+        <DescriptionWrap>
             <DescriptionField content={game.thumbnail} image />
-            <DescriptionField label={'Title'} content={game.title} />
+            <DescriptionField label="Title" content={game.title} />
             <DescriptionField
-                label={'Release date'}
+                label="Release date"
                 content={normalizeDate(game.release_date)}
             />
-            <DescriptionField label={'Publisher'} content={game.publisher} />
-            <DescriptionField label={'Developer'} content={game.developer} />
-            <DescriptionField label={'Genre'} content={game.genre} />
+            <DescriptionField label="Publisher" content={game.publisher} />
+            <DescriptionField label="Developer" content={game.developer} />
+            <DescriptionField label="Genre" content={game.genre} />
 
-            {/*<DescriptionField*/}
-            {/*    label={'System requirements'}*/}
-            {/*    content={game.minimum_system_requirements.}*/}
-            {/*/>*/}
-        </DesriptionWrap>
+            <SystemReqDescription
+                requirements={game.minimum_system_requirements}
+            />
+        </DescriptionWrap>
     )
 }
 
---
+++ src/pages/GameDetailsPage/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const DescriptionWrap = styled.ul`
+    padding: 0;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    max-width: 600px;
+`
